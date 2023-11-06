fddc615ae9af199b94c94f355c7b463c9aa9c59e
fifthfir <ruotianz@FifthFirE>
2023-11-06 10:44:50 -0800

    31

--- learn2prog/30_sort_lines/sortLines.c
+++ learn2prog/30_sort_lines/sortLines.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+void printData(char ** sortedData, size_t count) { //new
+	for (size_t i = 0; i < count; i++) {
+		printf("%s", sortedData[i]);
+		free(sortedData[i]);
+	}
+}
 
 //This function is used to figure out the ordering
-//of the strings in qsort.  You do not need
-//to modify it.
+//of the strings in qsort. You do not need to modify it.
 int stringOrder(const void * vp1, const void * vp2) {
 	const char * const * p1 = vp1;
 	const char * const * p2 = vp2;
@@ -17,7 +22,44 @@ void sortData(char ** data, size_t count) {
 	qsort(data, count, sizeof(char *), stringOrder);
 }
 
-int main(int argc, char ** argv) {
-	//WRITE YOUR CODE HERE!
+void loadData(FILE * f) { //new
+	char ** array = NULL;
+	char * line = NULL;
+	size_t sz = 0;
+	size_t i = 0;
+
+	while (getline(&line, &sz, f) >= 0) {
+		array = realloc(array, sz * sizeof(*array) + sizeof(*array));
+		array[i] = line;
+		line = NULL ;
+		i++;
+	}
+
+	free(line);
+	sortData(array, i);
+	printData(array, i);
+	free(array);
+}
+
+int main(int argc, char ** argv) { //new
+	if (argc == 1) {
+		loadData(stdin);
+	}else if (argc > 1) {
+		for (int i = 1; i < argc; i++) {
+			FILE * f = fopen(argv[i], "r");
+			if (f == NULL) {
+				fprintf(stderr, "File not found\n");
+				return EXIT_FAILURE;
+			}
+			loadData(f);
+			if (fclose(f) != 0) {
+				fprintf(stderr, "File could not close\n");
+				return EXIT_FAILURE;
+			}
+		}
+	}else {
+		fprintf(stderr, "no input file");
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
